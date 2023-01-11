38d39d22e0ba50c9d7e616ec3d2d9413ff292b85
Suraj Shrestha <suraj.shrestha@xrdig.com>
2023-01-11 14:02:55 +0545

    added simple login for microsoft graph toolkit and retrive local files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Login, Agenda, Person, ViewType, People, File, FileList } from "@microsoft/mgt-react";
+import { Providers, ProviderState } from "@microsoft/mgt-element";
+
+const personDetails = {
+  displayName: "Dinesh Poudel",
+  mail: "dinesh.poudel@xrdig.com",
+};
+
+function useIsSignedIn(): [boolean] {
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const updateState = () => {
+      const provider = Providers.globalProvider;
+      setIsSignedIn(provider && provider.state === ProviderState.SignedIn);
+    };
+    Providers.onProviderUpdated(updateState);
+    updateState();
+    return () => {
+      Providers.removeProviderUpdatedListener(updateState);
+    };
+  }, []);
+  return [isSignedIn];
+}
 
 function App() {
+  const [isSignedIn] = useIsSignedIn();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Login />
       </header>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {isSignedIn && (
+          <div style={{ width: "1000px", display: 'flex' }}>
+            <Agenda />
+           <FileList fileListQuery="me/drive/root/children" style={{ width: '500px'}} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Providers } from '@microsoft/mgt-element';
+import { Msal2Provider } from '@microsoft/mgt-msal2-provider';
+import { LoginType } from '@microsoft/mgt-element';
+import { PromptType } from '@microsoft/mgt-msal2-provider';
+
+Providers.globalProvider = new Msal2Provider({
+  clientId: '0808810d-df53-47e6-8439-713e332a4ff4',
+  scopes: ['calendars.read', 'user.read', 'openid', 'profile', 'people.read', 'user.readbasic.all', 'files.read'],
+  loginType: LoginType.Popup,
+  prompt: PromptType.CONSENT,
+  isIncrementalConsentDisabled: true,
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
